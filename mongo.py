b9fd28c5f205e16453fb4b0408bc3d65a4574a48
Yuqiwu <ywu@stuy.edu>
2018-02-14 20:39:15 -0500

    test

--- mongo.py
+++ mongo.py
@@ -8,17 +8,17 @@ def find_borough(b):
     name = []
     all = collection.find( {"borough": b} )
     for each in all:
-        name.append(each["name"])
+        name.append(each)
     return name
 
 def find_zpcode(z):
     name = []
     all = collection.find( {"address.zipcode": z} )
     for each in all:
-        name.append(each["name"])
+        name.append(each)
     return name
 
-def find_grade_zpc(z, g):
+def find_zpc_grade(z, g):
     name = []
     all = collection.find( {"address.zipcode": z}, {"grades.grade": g} )
     for each in all:
@@ -26,13 +26,13 @@ def find_grade_zpc(z, g):
         name.append(one["name"])
     return name
 
-def find_score_zpc(z, g):
-    name = []
+def find_zpc_score(z, g):
+    info = []
     all = collection.find( {"address.zipcode": z}, {"grades.score": g} )
     for each in all:
         one = collection.find_one({"_id" : each["_id"]})
-        name.append(one["name"])
+        info.append(one)
     return name
 
 
-print find_grade_zpc("11225", "A")
+print find_borough("Brooklyn")
